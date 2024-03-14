a3764118a9e774a8e0a6b157851225b62b5285a9
Kaija Gahm <kaija.gahm@gmail.com>
2024-03-14 14:57:44 -0700

    added normDegree and normStrength to network metrics. Updated graph_fn

--- scripts/temporal_funs.R
+++ scripts/temporal_funs.R
@@ -22,3 +22,42 @@ plotRepeat <- function(df, var, situation = "flight", ylab){
 }
 
 cols_to_remove <- c("tag_id", "sensor_type_id", "acceleration_raw_x", "acceleration_raw_y", "acceleration_raw_z", "barometric_height", "battery_charge_percent", "battery_charging_current", "external_temperature", "gps_hdop", "gps_satellite_count", "gps_time_to_fix", "import_marked_outlier", "light_level", "magnetic_field_raw_x", "magnetic_field_raw_y", "magnetic_field_raw_z", "ornitela_transmission_protocol", "tag_voltage", "update_ts", "visible", "deployment_id", "event_id", "sensor_type", "tag_local_identifier", "location_long.1", "location_lat.1", "optional", "sensor", "earliest_date_born", "exact_date_of_birth", "group_id", "individual_id", "latest_date_born", "local_identifier", "marker_id", "mates", "mortality_date", "mortality_latitude", "mortality_type", "nick_name", "offspring", "parents", "ring_id", "siblings", "taxon_canonical_name", "taxon_detail", "number_of_events", "number_of_deployments")
+
+# Function to get degree and strength
+getmetrics <- function(graph, interval, type, days){
+  if(length(graph) > 0){
+    metrics <- data.frame(degree = igraph::degree(graph),
+                          strength = igraph::strength(graph),
+                          Nili_id = names(igraph::degree(graph)),
+                          int = interval,
+                          n = length(V(graph)),
+                          type = type,
+                          ndays = days) %>%
+      mutate(normDegree = degree/(n-1),
+             normStrength = strength/sum(strength))
+  }else{
+    metrics <- data.frame(degree = NA, strength = NA, 
+                          Nili_id = NA, int = interval, 
+                          n = 0, type = type, ndays = days)
+  }
+  
+  return(metrics)
+}
+
+graph_fn <- function(graph, layout, title, bbox){
+    g <- ggraph(graph, layout) +
+      geom_edge_link(alpha = 0.3)+
+      geom_node_point(aes(col = normDegree, size = normDegree))+
+      geom_node_text(aes(label = name), 
+                     col = "white", 
+                     size = 2)+
+      scale_color_viridis_c(limits = c(0, 1))+
+      scale_size_continuous(limits = c(0, 1))+
+      theme_void()+
+      xlim(bbox$xmin[1], bbox$xmax[1])+
+      ylim(bbox$ymin[1], bbox$ymax[1])+
+      theme(legend.position = "none",
+            panel.background = element_rect(fill = "gray80", color = "gray80"))+
+      ggtitle(title)
+    return(g)
+}
