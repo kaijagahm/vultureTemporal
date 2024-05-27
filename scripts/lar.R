39a7984fcef96c64493161ebdda4d6d6840d4cf2
Kaija Gahm <kaija.gahm@gmail.com>
2024-05-27 15:41:54 -0700

    Changed to 5-day windows and allowed double-sided dyads

--- scripts/lar.R
+++ scripts/lar.R
@@ -26,83 +26,92 @@ dim(inds) #151 inds, with info about each individual.
 
 # playing around with the vulture data
 tar_load(flight_sris)
-## Let's take the 1-day windows first, since they're most detailed.
-oneday <- flight_sris[[1]]
-days <- purrr::imap(oneday, ~.x %>% mutate(day = .y)) %>% purrr::list_rbind() %>%
+## Let's take the 5-day windows
+fiveday <- flight_sris[[2]]
+periods <- purrr::imap(fiveday, ~.x %>% mutate(period = .y)) %>% purrr::list_rbind() %>%
   mutate(dyad = paste(ID1, ID2, sep = ", "))
 
-alldyads <- unique(days$dyad)
-alldays <- 1:max(days$day)
-alldyaddays <- expand_grid("dyad" = alldyads, "day" = alldays)
-days <- alldyaddays %>% left_join(days) %>% # this adds in all the NA's for the dyads that didn't interact on certain days.
+alldyads <- unique(periods$dyad)
+allperiods <- 1:max(periods$period)
+alldyadperiods <- expand_grid("dyad" = alldyads, "period" = allperiods)
+periods <- alldyadperiods %>% left_join(periods) %>% # this adds in all the NA's for the dyads that didn't interact during certain periods
   mutate(ID1 = str_extract(dyad, ".*(?=\\,)"),
          ID2 = str_extract(dyad, "(?<=\\,\\s).*"))
 
-firstinteractions <- days %>%
-  group_by(dyad) %>%
+firstinteractions <- periods %>%
+  group_by(period) %>%
   filter(weight > 0 & !is.na(weight)) %>%
-  arrange(day, .by_group = T) %>%
+  arrange(period, .by_group = T) %>%
   slice(1) %>%
   ungroup()
 
 # Ok but, first interactions are arbitrary. And when we ask "how long does an association last," we also care about differentiating the associations from random. It can't be considered an association if it's due to random noise. However, when we're talking about time, we really can't use the same wrap-around method, because that messes up time. So maybe we're really concerned with a social network permutation? We want to ask whether a given dyad differs significantly in strength from random dyads? I think that brings us back to identity swapping. 
 
 # What if we binarize all of this? So make it just a sequence of 0's and 1's, which I think Elvira already did?
-days_bin <- days %>%
+periods_bin <- periods %>%
   mutate(yn = case_when(is.na(weight) ~ NA,
                         weight > 0 & !is.na(weight) ~ 1,
                         weight == 0 & !is.na(weight) ~ 0))%>%
-  select(ID1, ID2, dyad, day, yn)
+  select(ID1, ID2, dyad, period, yn)
 
-leads <- 1:50
+periods_bin_rev <- periods_bin
+names(periods_bin_rev)[1:2] <- c("ID2", "ID1")
+periods_bin_rev <- periods_bin_rev %>%
+  mutate(dyad = paste(ID1, ID2, sep = ", "))
 
-# Next to do: do this on different time scales (hourly should show day/night patterns at the very least, 5-day increments, etc.)
+periods_bin_all <- bind_rows(periods_bin, periods_bin_rev) # add on the opposite dyads
+periods_bin_all <- periods_bin_all %>%
+  rowwise() %>%
+  mutate(dyad_absolute = paste(min(ID1, ID2), max(ID1, ID2), sep = ", ")) %>%
+  ungroup()
+
 # Also evaluate whether this measure makes sense
 # Is there a way to make a weighted version of this instead of just y/n?
 # Maybe this still isn't really LAR. This is more "given they interact, what is the probability that they interact the next day?" instead of "given they interact, what is the probability that B is one of A's partners the next day?"--which should account for overall lower association rates the following day. Let's try that, both weighted and unweighted, next.
 
 # Let's try reinterpreting: given that A and B interact on a given day, what is the probability of B being one of A's associates after a time lag t?
-# XXX need to do this with the data doubled since we're looking on an individual basis--LAR's won't be symmetrical for a given dyad.
-daily_strengths <- days_bin %>%
-  group_by(ID1, day) %>%
-  summarize(str_day = sum(yn == 1, na.rm = T),
-            n_day = sum(!is.na(yn)))
+# XXX need to do this with the data doubled since we're looking on an individual basis--LAR's won't be symmetrical for a given dyad
+period_strengths <- periods_bin_all %>%
+  group_by(ID1, period) %>%
+  summarize(str_period = sum(yn == 1, na.rm = T),
+            n_period = sum(!is.na(yn)))
 
-prob_assoc <- days_bin %>%
-  arrange(dyad, day) %>%
+prob_assoc <- periods_bin_all %>%
+  arrange(dyad, period) %>%
   group_by(dyad) %>%
   mutate(lead_value = lead(yn, 1),
-         lead_day = lead(day, 1)) %>%
+         lead_period = lead(period, 1)) %>%
   ungroup() %>%
-  left_join(daily_strengths, by = c("ID1", "lead_day" = "day")) %>%
-  rename("str_lead_day_id1" = "str_day",
-         "n_lead_day_id1" = "n_day") %>%
-  mutate(prob_assoc = lead_value/str_lead_day_id1) %>%
+  left_join(period_strengths, by = c("ID1", "lead_period" = "period")) %>%
+  rename("str_lead_period_id1" = "str_period",
+         "n_lead_period_id1" = "n_period") %>%
+  mutate(prob_assoc = lead_value/str_lead_period_id1) %>%
   filter(yn == 1)
+
+levels <- prob_assoc %>%
+  filter(!is.na(prob_assoc)) %>%
+  group_by(dyad) %>%
+  filter(n() >= 7) %>%
+  summarize(mn = mean(prob_assoc)) %>% 
+  arrange(desc(mn)) %>%
+  pull(dyad)
 
 prob_assoc %>%
   filter(!is.na(prob_assoc)) %>%
   group_by(dyad) %>%
-  summarize(n = n(),
-            mnprob = mean(prob_assoc),
-            sdprob = sd(prob_assoc)) %>%
-  filter(n > 4) %>%
-  arrange(desc(mnprob)) %>%
-  mutate(dyad = factor(dyad, levels = as.character(dyad))) %>%
-  ggplot(aes(x = dyad, y = mnprob))+
-  geom_point()+
-  geom_errorbar(aes(ymax = mnprob + sdprob,
-                    ymin = mnprob - sdprob),
-                width = 0)+
+  filter(n() >=7) %>%
+  mutate(dyad = factor(dyad, levels = levels)) %>%
+  ggplot(aes(x = dyad, y = prob_assoc, fill = factor(dyad_absolute)))+
+  geom_boxplot(outlier.size = 0.5)+
   theme_minimal()+
-  theme(axis.text.x = element_blank(),
-        panel.grid.major.x = element_blank())+
-  ylab("Prob randomly-selected assocate \nof A is B after 1 day")+
+  theme(panel.grid.major.x = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "none")+
+  ylab("Prob randomly-selected assocate \nof A is B after 5 days")+
   xlab("Dyad")+
-  labs(caption = "(includes dyads with >= 5 valid lags)")+
-  geom_hline(aes(yintercept = 0), lty = 3, col = "red")
-# There is a lot of missing data here--many dyads do not have re-association data
-# Obviously going below 0 doesn't make sense, oops, should do this as a boxplot instead.
+  labs(caption = "(includes dyads with >= 6 valid lags)")
+# Note that the dyads are not always next to each other, because these relationships are not symmetrical.
+
 # How does jackknifing fit in here?
 # Is this even the right measure?
 # Can we make it weighted?
