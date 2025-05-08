58cfa8a1719dc32bf2c5a3e6fa9a0d3fd7972afb
Kaija Gahm <kaija.gahm@gmail.com>
2025-05-08 12:23:02 -0700

    remove years analysis

--- _targets.R
+++ _targets.R
@@ -1,8 +1,3 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
 # Load packages required to define the pipeline:
 library(targets)
 # library(tarchetypes) # Load other packages as needed.
@@ -10,38 +5,6 @@ library(targets)
 # Set target options:
 tar_option_set(
   packages = c("tibble", "purrr", "sf", "igraph", "muxViz", "Matrix", "dplyr") # Packages that your targets need for their tasks.
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -63,26 +26,6 @@ list(
   tar_target(roostPolygons, st_read(roostPolygons_file)),
   
   # Time window analysis
-  #YEARS -------------------------------------------------------------------
-  tar_target(data_years, function(alldata_prepped){
-    yr_20_21 <- purrr::list_rbind(alldata_prepped[1:3])
-    yr_21_22 <- purrr::list_rbind(alldata_prepped[4:6])
-    yr_22_23 <- purrr::list_rbind(alldata_prepped[7:9])
-    yrs <- list("yr_20_21" = yr_20_21,
-                "yr_21_22" = yr_21_22,
-                "yr_22_23" = yr_22_23)
-    return(yrs)
-  }),
-  tar_target(roosts_years, function(roosts){
-    r_20_21 <- purrr::list_rbind(roosts[1:3])
-    r_21_22 <- purrr::list_rbind(roosts[4:6])
-    r_22_23 <- purrr::list_rbind(roosts[7:9])
-    rs <- list("r_20_21" = r_20_21,
-               "r_21_22" = r_21_22,
-               "r_22_23" = r_22_23)
-    return(rs)
-  }), # will come back to this; can't really do reducibility analysis on just three slices.
-  
   # SEASONS -----------------------------------------------------------------
   tar_target(data_seasons, alldata_prepped), # just renaming these; unnecessary step
   tar_target(roosts_seasons, roosts),
