3b14f9201e52eea0a07b70151db669c0f80cd3a9
Kaija Gahm <kaija.gahm@gmail.com>
2025-06-18 10:46:37 -0700

    more figures

--- _targets.R
+++ _targets.R
@@ -5,7 +5,8 @@ library(crew)
 
 # Set target options:
 tar_option_set(
-  packages = c("tidyverse", "tibble", "purrr", "sf", "igraph", "muxViz", "Matrix", "dplyr", "vultureUtils")#, # Packages that your targets need for their tasks.
+  packages = c("tidyverse", "tibble", "purrr", "sf", "igraph", "muxViz", "Matrix", "dplyr", "vultureUtils"), # Packages that your targets need for their tasks.
+  controller = crew_controller_local(workers = 5)
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -17,12 +18,11 @@ list(
   # Reducibility analysis
   ## General data ingest and prep
   tar_target(cols_to_remove, c("tag_id", "sensor_type_id", "acceleration_raw_x", "acceleration_raw_y", "acceleration_raw_z", "barometric_height", "battery_charge_percent", "battery_charging_current", "external_temperature", "gps_hdop", "gps_satellite_count", "gps_time_to_fix", "import_marked_outlier", "light_level", "magnetic_field_raw_x", "magnetic_field_raw_y", "magnetic_field_raw_z", "ornitela_transmission_protocol", "tag_voltage", "update_ts", "visible", "deployment_id", "event_id", "sensor_type", "tag_local_identifier", "location_long.1", "location_lat.1", "optional", "sensor", "earliest_date_born", "exact_date_of_birth", "group_id", "individual_id", "latest_date_born", "local_identifier", "marker_id", "mates", "mortality_date", "mortality_latitude", "mortality_type", "nick_name", "offspring", "parents", "ring_id", "siblings", "taxon_canonical_name", "taxon_detail", "number_of_events", "number_of_deployments")),
-  tar_target(alldata_file, "data/fromMvmtSoc/downsampled_10min_forSocial.Rda", format = "file"),
-  tar_target(alldata, read_from_path(alldata_file)),
+  tar_target(tarstorepath, "~/Desktop/projects/MvmtSoc/_targets/"),
+  tar_target(alldata, targets::tar_read("downsampled_10min_forSocial", store = tarstorepath)),
   tar_target(season_names, map_chr(alldata, ~as.character(.x$seasonUnique[1]))),
   tar_target(alldata_prepped, map(alldata, ~prepare_data(.x, cols_to_remove = cols_to_remove))),
-  tar_target(roosts_file, "data/roosts.Rda", format = "file"),
-  tar_target(roosts, read_from_path(roosts_file)),
+  tar_target(roosts, targets::tar_read("roosts", store = tarstorepath)),
   tar_target(roostPolygons_file, "data/raw/roosts50_kde95_cutOffRegion.kml", format = "file"),
   tar_target(roostPolygons, st_read(roostPolygons_file)),
   
@@ -64,13 +64,21 @@ list(
 
   ### Cut data
   tar_target(timewindows, c(1, 5, 10, 25)),
-  tar_target(timewindows_heuristic, seq(from = 1, to = 50, by = 5)),
+  tar_target(timewindows_heuristic, c(1, 3, 5, 7, seq(from = 10, to = 25, by = 5))),
   tar_target(data_cut, cut_data(summer2023data, timewindows)),
   tar_target(data_cut_heuristic, cut_data(summer2023data, timewindows_heuristic)),
   ### Prepare edges
   tar_target(flight_sris, map(data_cut, ~get_flight_sris(.x, roostPolygons))),
   tar_target(feeding_sris, map(data_cut, ~get_feeding_sris(.x, roostPolygons))),
-  tar_target(flight_sris_heuristic, map(data_cut_heuristic, ~get_flight_sris(.x, roostPolygons))),
+  tar_target(fsh1, get_flight_sris(data_cut_heuristic[[1]], roostPolygons)),
+  tar_target(fsh2, get_flight_sris(data_cut_heuristic[[2]], roostPolygons)),
+  tar_target(fsh3, get_flight_sris(data_cut_heuristic[[3]], roostPolygons)),
+  tar_target(fsh4, get_flight_sris(data_cut_heuristic[[4]], roostPolygons)),
+  tar_target(fsh5, get_flight_sris(data_cut_heuristic[[5]], roostPolygons)),
+  tar_target(fsh6, get_flight_sris(data_cut_heuristic[[6]], roostPolygons)),
+  tar_target(fsh7, get_flight_sris(data_cut_heuristic[[7]], roostPolygons)),
+  tar_target(fsh8, get_flight_sris(data_cut_heuristic[[8]], roostPolygons)),
+  tar_target(flight_sris_heuristic, list(fsh1, fsh2, fsh3, fsh4, fsh5, fsh6, fsh7, fsh8)),
   tar_target(feeding_sris_heuristic, map(data_cut_heuristic, ~get_feeding_sris(.x, roostPolygons))),
   tar_target(allvertices, map(data_cut, ~unique(list_rbind(.x)$Nili_id))),
   tar_target(allvertices_heuristic, map(data_cut_heuristic, ~unique(list_rbind(.x)$Nili_id))),
@@ -104,41 +112,41 @@ list(
   tar_target(curves, purrr::list_rbind(list(
     get_reduc_curves_df(red_flight_cat, timewindows, "categorical", "flight"),
     get_reduc_curves_df(red_feeding_cat, timewindows, "categorical", "feeding")
-  ))),
+  )))#,
   
   ## Dyads
-  tar_target(fe_5days_fordyads, prep(feeding_sris[[2]], "feeding")),
-  tar_target(fl_5days_fordyads, prep(flight_sris[[2]], "flight")),
-  tar_target(all_5days_fordyads, 
-             list_rbind(list(fe_5days_fordyads, fl_5days_fordyads)) %>% 
-               mutate(dyad = paste(ID1, ID2, sep = ", ")) %>%
-               group_by(dyad, situ) %>%
-               mutate(n_dyad_situ = n(),
-                      n_dyad_situ_nonzero = sum(weight > 0)) %>%
-               ungroup() %>%
-               group_by(dyad) %>%
-               mutate(n_dyad = n(),
-                      n_dyad_nonzero = sum(weight > 0))),
-  tar_target(sri_dist_fl, pull(fl_5days_fordyads, weight)),
-  tar_target(sri_dist_fe, pull(fe_5days_fordyads, weight)),
-  tar_target(static_fl, fl_5days_fordyads %>% 
-               select(ID1, ID2, situ, period) %>% 
-               mutate(weight = sample(sri_dist_fl))),
-  tar_target(static_fe, fe_5days_fordyads %>% 
-               select(ID1, ID2, situ, period) %>% 
-               mutate(weight = sample(sri_dist_fe))),
-  tar_target(static, bind_rows(static_fl, static_fe) %>%
-               mutate(dyad = paste(ID1, ID2, sep = ", "))),
-  tar_target(gs, get_networks_dyads(all_5days_fordyads)),
-  tar_target(gs_static, get_networks_dyads(static)),
-  tar_target(shuffled_reps_df, get_shuffled_reps_df(graphs = gs, reps = 100)),
-  tar_target(shuffled_reps_static_df, get_shuffled_reps_df(graphs = gs_static, reps = 100)),
-  tar_target(replicates, correct_dyad_id_order(shuffled_reps_df)),
-  tar_target(replicates_static, correct_dyad_id_order(shuffled_reps_static_df)),
-  tar_target(lms_obs_summ, get_lms(all_5days_fordyads)),
-  tar_target(lms_obs_summ_static, get_lms(static)),
-  tar_target(lms_perm_summ, get_lms_permuted(replicates, workers = 20)),
-  tar_target(lms_perm_summ_static, get_lms_permuted(replicates_static, workers = 20)),
-  tar_target(combined_obs, get_combined(all_5days_fordyads, lms_obs_summ, lms_perm_summ)),
-  tar_target(combined_static, get_combined(static, lms_obs_summ_static, lms_perm_summ_static))
+  # tar_target(fe_5days_fordyads, prep(feeding_sris[[2]], "feeding")),
+  # tar_target(fl_5days_fordyads, prep(flight_sris[[2]], "flight")),
+  # tar_target(all_5days_fordyads, 
+  #            list_rbind(list(fe_5days_fordyads, fl_5days_fordyads)) %>% 
+  #              mutate(dyad = paste(ID1, ID2, sep = ", ")) %>%
+  #              group_by(dyad, situ) %>%
+  #              mutate(n_dyad_situ = n(),
+  #                     n_dyad_situ_nonzero = sum(weight > 0)) %>%
+  #              ungroup() %>%
+  #              group_by(dyad) %>%
+  #              mutate(n_dyad = n(),
+  #                     n_dyad_nonzero = sum(weight > 0))),
+  # tar_target(sri_dist_fl, pull(fl_5days_fordyads, weight)),
+  # tar_target(sri_dist_fe, pull(fe_5days_fordyads, weight)),
+  # tar_target(static_fl, fl_5days_fordyads %>% 
+  #              select(ID1, ID2, situ, period) %>% 
+  #              mutate(weight = sample(sri_dist_fl))),
+  # tar_target(static_fe, fe_5days_fordyads %>% 
+  #              select(ID1, ID2, situ, period) %>% 
+  #              mutate(weight = sample(sri_dist_fe))),
+  # tar_target(static, bind_rows(static_fl, static_fe) %>%
+  #              mutate(dyad = paste(ID1, ID2, sep = ", "))),
+  # tar_target(gs, get_networks_dyads(all_5days_fordyads)),
+  # tar_target(gs_static, get_networks_dyads(static)),
+  # tar_target(shuffled_reps_df, get_shuffled_reps_df(graphs = gs, reps = 100)),
+  # tar_target(shuffled_reps_static_df, get_shuffled_reps_df(graphs = gs_static, reps = 100)),
+  # tar_target(replicates, correct_dyad_id_order(shuffled_reps_df)),
+  # tar_target(replicates_static, correct_dyad_id_order(shuffled_reps_static_df)),
+  # tar_target(lms_obs_summ, get_lms(all_5days_fordyads)),
+  # tar_target(lms_obs_summ_static, get_lms(static)),
+  # tar_target(lms_perm_summ, get_lms_permuted(replicates, workers = 20)),
+  # tar_target(lms_perm_summ_static, get_lms_permuted(replicates_static, workers = 20)),
+  # tar_target(combined_obs, get_combined(all_5days_fordyads, lms_obs_summ, lms_perm_summ)),
+  # tar_target(combined_static, get_combined(static, lms_obs_summ_static, lms_perm_summ_static))
 )
--- scripts/reducibility_curves.R
+++ scripts/reducibility_curves.R
@@ -1,6 +1,7 @@
 library(targets)
 library(tidyverse)
 library(dendextend)
+library(here)
 source("R/functions.R")
 tar_load(curves)
 tar_load(curves_seasons)
@@ -20,78 +21,110 @@ theme_set(theme_minimal())
 ## Seasons
 plt_curves_seasons <- curves_seasons %>%
   ggplot(aes(x = step, y = ent_norm, col = situ))+
+  annotate(geom = "segment", x = 1, xend = 9, y = 1, yend = 0, alpha = 0.7, col = "black", linetype = 3)+
   geom_point(size = 2, alpha = 0.7)+
-  geom_line(linewidth = 1, alpha = 0.7)+
+  geom_line(linewidth = 1, alpha = 0.7, aes(linetype = situ))+
   scale_color_manual(name = "Situation", values = situcolors[1:2])+
-  ylab("Relative entropy (normalized)") + xlab("Amount of aggregation")+
+  scale_linetype_manual(name = "Situation", values = c(1, 2))+
+  ylab("Relative entropy (normalized)") + xlab("Layers aggregated")+
   scale_x_continuous(breaks = 1:9)+
+  theme_classic()+
   theme(panel.grid.minor = element_blank(),
         text = element_text( size = 14))+
   ggtitle("Seasons")+
   guides(color = guide_legend(override.aes = 
                                 list(linewidth = 1, size = 3)))
-ggsave(plt_curves_seasons, file = here("fig/plt_curves_seasons.png"), width = 6, height = 4)
+plt_curves_seasons
+ggsave(plt_curves_seasons, file = here("fig/plt_curves_seasons.png"), width = 6, height = 3)
 
 ## Days
+lines <- curves %>% group_by(timewindow) %>% summarize(xend = max(step)) %>%
+  mutate(x = 1, y = 1, yend = 0)
 plt_curves_days <- curves %>% 
   mutate(timewindow = factor(timewindow, levels = c(1, 5, 10, 25))) %>%
-  ggplot(aes(x = timestep, y = ent_norm, col = situ))+
+  ggplot(aes(x = step, y = ent_norm, col = situ))+
+  geom_segment(data = lines, aes(x = x, xend = xend, y = y, yend = yend), alpha = 0.7, col = "black", linetype = 3)+
   geom_point(alpha = 0.7)+
   geom_line(alpha = 0.7)+
   facet_wrap(~timewindow, scales = "free")+
   theme_classic()+
-  xlab("Amount of aggregation")+
+  xlab("Layers aggregated")+
   ylab("Relative entropy (normalized)")+
   scale_color_manual(name = "Situation", values = situcolors[1:2])+
   theme(text = element_text(size = 14))+
   guides(color = guide_legend(override.aes = 
                                 list(linewidth = 1, size = 3)))
+plt_curves_days
 ggsave(plt_curves_days, file = here("fig/plt_curves_days.png"), width = 6, height = 4)
 
 # Heat maps ---------------------------------------------------------------
 ## Let's start with seasons so it's more manageable (feeding only)
 mat_fe <- provideDimnames(as.matrix(red_feeding_cat_seasons$JSD), sep = "",
                           list(season_names, season_names))
+mat_fl <- provideDimnames(as.matrix(red_flight_cat_seasons$JSD), sep = "",
+                          list(season_names, season_names))
 
-## automatic
-gplots::heatmap.2(mat_fe, trace = "none", Rowv = TRUE, Colv = TRUE, dendrogram = "row", density.info = "density", key = FALSE, main = "", margins = c(10, 10)) # ok, so at first glance, this looks unordered, but we can actually order it without changing the information it contains, I *think*??
+## automatic (feeding)
+#gplots::heatmap.2(mat_fe, trace = "none", dendrogram = "row", key = FALSE, margins = c(10, 10)) # at first glance, this looks unordered, but I think we can actually rotate the nodes and keep the dendrogram
 
-## automatic ordered
-gplots::heatmap.2(mat_fe, trace = "none", Rowv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), Colv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), dendrogram = "row", density.info = "density", key = FALSE, main = "", margins = c(10, 10)) 
+## automatic (flight)
+gplots::heatmap.2(mat_fl, trace = "none", dendrogram = "row", key = FALSE, margins = c(10, 10))
 
-## forcibly ordered (without dendrogram)
-gplots::heatmap.2(mat_fe, trace = "none", Rowv = season_names, Colv = season_names, dendrogram = "none", density.info = "density", key = FALSE, main = "", margins = c(10, 10)) # hmm but now this is going the other way!
+## automatic ordered (feeding) (i.e. enforcing the order as much as possible while rotating the existing dendrogram)
+# gplots::heatmap.2(mat_fe, trace = "none", Rowv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), Colv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), dendrogram = "column", key = FALSE, margins = c(10, 10)) 
 
+## automatic ordered (flight)
 
-## ordered
-gplots::heatmap.2(mat_fe, trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key = FALSE, main = "", margins = c(10, 10)) 
+season_names_abbreviated <- str_remove(season_names, "_")
+season_names_abbreviated <- str_replace(season_names_abbreviated, "breeding", "B")
+season_names_abbreviated <- str_replace(season_names_abbreviated, "fall", "F")
+season_names_abbreviated <- str_replace(season_names_abbreviated, "summer", "S")
+season_names_abbreviated <- str_remove(season_names_abbreviated, "20")
+colnames(mat_fl) <- season_names_abbreviated
+rownames(mat_fl) <- season_names_abbreviated
 
-gplots::heatmap.2(mat_fe, trace = "none", Rowv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), Colv = rotate(as.dendrogram(hclust(as.dist(mat_fe))), order = season_names), dendrogram = "row", density.info = "density", key = FALSE, main = "", margins = c(10, 10)) 
+# --- Heatmap 1: Original with dendrogram (as per your reprex) ---
+gplots::heatmap.2(mat_fl, trace = "none", dendrogram = "row", key = FALSE,
+                  margins = c(10, 10))
+
+# --- Heatmap 2: Dendrogram rotated as close as possible to temporal order (as per your reprex) ---
+gplots::heatmap.2(mat_fl, trace = "none",
+                  Rowv = rotate(as.dendrogram(hclust(as.dist(mat_fl))), order = season_names_abbreviated),
+                  Colv = rotate(as.dendrogram(hclust(as.dist(mat_fl))), order = season_names_abbreviated),
+                  dendrogram = "column", 
+                  key = TRUE, margins = c(10, 10), density.info = "none", key.title = "", key.xlab= "", key.ylab = "", keysize = 2)
+
+# --- Heatmap 3: No dendrogram, forced temporal order, matching reflection ---
+gplots::heatmap.2(mat_fl[rev(season_names_abbreviated), season_names_abbreviated], 
+                  trace = "none",
+                  dendrogram = "none", # No dendrogram
+                  key = FALSE,
+                  Rowv = NA,           # Prevent further reordering by Rowv
+                  Colv = NA,           # Prevent further reordering by Colv
+                  margins = c(10, 10), density.info = "none", key.title = "", key.xlab= "", key.ylab = "", keysize = 2)
+
+season_order <- c("20F", "21F", "22F", "21B", "22B", "23B", "21S", "22S", "23S")
+gplots::heatmap.2(mat_fl[rev(season_order), season_order], 
+                  trace = "none",
+                  dendrogram = "none", # No dendrogram
+                  key = FALSE,
+                  Rowv = NA,           # Prevent further reordering by Rowv
+                  Colv = NA,           # Prevent further reordering by Colv
+                  margins = c(10, 10), density.info = "none", key.title = "", key.xlab= "", key.ylab = "", keysize = 2)
 
 # So it is not the case that we see a seasonal signature--if that was true, then we'd expect the patterns to be more similar between seasons of the same type than between adjacent seasons. What we see is that one season basically follows from the last.
 
-## Okay, now the same for some of the daily ones
-mats_fl <- map(red_flight_cat, ~as.matrix(.x$JSD) %>%
-                 provideDimnames(., sep = "", list(as.character(1:length(.x$JSD)), 
-                                                   as.character(1:length(.x$JSD)))))
+## Okay now let's do a daily one
 mats_fe <- map(red_feeding_cat, ~as.matrix(.x$JSD) %>%
-                 provideDimnames(., sep = "", list(as.character(1:length(.x$JSD)), 
+                 provideDimnames(., sep = "", list(as.character(1:length(.x$JSD)),
                                                    as.character(1:length(.x$JSD)))))
-## Let's see first about 25-day windows and then go backwards
-
-### 25-day windows
-gplots::heatmap.2(mats_fl[[4]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-flight\n(25-day windows)") # interesting! We do not recover timescale from these.
-
-gplots::heatmap.2(mats_fe[[4]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-feeding\n(25-day windows)")  # but we do here...
-
-### 10-day windows
-gplots::heatmap.2(mats_fl[[3]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-flight\n(10-day windows)") # ish
-gplots::heatmap.2(mats_fe[[3]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-feeding\n(10-day windows)")
-
-### 5-day windows
-gplots::heatmap.2(mats_fl[[2]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-flight\n(5-day windows)") # ish
-gplots::heatmap.2(mats_fe[[2]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-feeding\n(5-day windows)")
-
 ### 1-day windows
-gplots::heatmap.2(mats_fl[[1]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-flight\n(1-day windows)") # ish
-gplots::heatmap.2(mats_fe[[1]], trace = "none", Rowv = FALSE, Colv = FALSE, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-feeding\n(1-day windows)")
+gplots::heatmap.2(mats_fe[[1]], trace = "none", Rowv = NA, Colv = NA, dendrogram = "none", density.info = "density", key.title = "", keysize = 2, key.xlab = "", key.ylab = "", main = "Co-feeding\n(1-day windows)")
+
+## How many individuals would we be left with if we restricted the seasonal analysis to only individuals that were present in all 9 seasons?
+tar_load(data_seasons)
+indivs_seasons <- map(data_seasons, ~.x %>% ungroup() %>% pull(Nili_id) %>% unique() %>% as_tibble()) %>% data.table::rbindlist(idcol = "season") %>% mutate(present = T) %>%
+  as_tibble() %>% pivot_wider(names_from = "season", values_from = "present", id_cols = "value")
+
+in_all <- na.omit(indivs_seasons)
+nrow(in_all) # only 20 individuals left!
