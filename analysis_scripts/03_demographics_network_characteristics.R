c574af45db5ea3a2be49803aee516b00bb5468de
Kaija Gahm <kaija.gahm@gmail.com>
2025-09-22 14:15:00 -0700

    begin script to describe networks

---
+++ analysis_scripts/03_demographics_network_characteristics.R
@@ -0,0 +1,11 @@
+# Demographics and descriptive measures of networks
+
+# Season-scale analysis ---------------------------------------------------
+tar_load(season_names) # vector of season names
+tar_load(data_seasons) # data split into seasons
+tar_load(graphs_flight_seasons) # flight graphs by season
+tar_load(graphs_feeding_seasons) # feeding graphs by season
+tar_load(indivs_in_all_seasons) # vector of individuals that are present in all seasons
+
+# Sub-seasonal scale analysis ---------------------------------------------
+tar_load(summer2023data) # data for summer 2023, for sub-seasonal analysis
